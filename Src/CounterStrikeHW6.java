98a1283b85c1c27bcb56860799c88982ce8f2027
VitaliiKozachyna <v.kozachyna@gmail.com>
2023-06-08 21:16:37 +0300

    ДЗ4(random)+ДЗ5+ДЗ6

--- Src/CounterStrikeHW6.java
+++ Src/CounterStrikeHW6.java
@@ -1,17 +1,20 @@
+import java.util.Random;
+
 public class CounterStrikeHW6 {
     public static void main(String[] args) {
         String teamA = "Команда-A";
         String teamB = "Команда-B";
-        int playerA1 = 5;
-        int playerA2 = 7;
-        int playerA3 = 10;
-        int playerA4 = 6;
-        int playerA5 = 5;
-        int playerB1 = 10;
-        int playerB2 = 2;
-        int playerB3 = 4;
-        int playerB4 = 7;
-        int playerB5 = 8;
+        Random random = new Random();
+        int playerA1 = random.nextInt(10);
+        int playerA2 = random.nextInt(10);
+        int playerA3 = random.nextInt(10);
+        int playerA4 = random.nextInt(10);
+        int playerA5 = random.nextInt(10);
+        int playerB1 = random.nextInt(10);
+        int playerB2 = random.nextInt(10);
+        int playerB3 = random.nextInt(10);
+        int playerB4 = random.nextInt(10);
+        int playerB5 = random.nextInt(10);
 
         double resultA = averageKills(playerA1, playerA2, playerA3, playerA4, playerA5);
         double resultB = averageKills(playerB1, playerB2, playerB3, playerB4, playerB5);
